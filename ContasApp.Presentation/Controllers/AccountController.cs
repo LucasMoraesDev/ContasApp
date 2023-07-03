3a2a32d4f47c16d6e36f26ac34a05d6a4e0e48d2
lcsmoraes <lms.262@gmail.com>
2023-07-03 10:57:29 -0300

    feat: Criação das páginas Login, Register e ForgotPassword

---
+++ ContasApp.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContasApp.Presentation.Controllers
+{
+    /// <summary>
+    /// /classe de controle do Asp.Net MVC
+    /// </summary>
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Método para abrir a página /Account/Login
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Método para abrir a página /Account/Register
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Método para abrir a página /Account/ForgotPassword
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
